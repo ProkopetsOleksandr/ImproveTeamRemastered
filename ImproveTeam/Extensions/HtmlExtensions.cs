439afc8db1c90826099d5a073d0bdb2df81d29bd
Oleksandr Prokopets <prokopets.alexandr@gmail.com>
2023-01-28 19:26:01 +0100

    Implement application layout

---
+++ ImproveTeam/Extensions/HtmlExtensions.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System.Linq;
+
+namespace ImproveTeam.Extensions
+{
+    public static class HtmlExtensions
+    {
+        public static string ActiveClass(this IHtmlHelper htmlHelper, string controllers = null, string actions = null, string cssClass = "active")
+        {
+            var currentController = htmlHelper?.ViewContext.RouteData.Values["controller"] as string;
+            var currentAction = htmlHelper?.ViewContext.RouteData.Values["action"] as string;
+
+            var acceptedControllers = (controllers ?? currentController ?? string.Empty).Split(',');
+            var acceptedActions = (actions ?? currentAction ?? string.Empty).Split(',');
+
+            if (acceptedControllers.Contains(currentController) &&
+                acceptedActions.Contains(currentAction))
+            {
+                return cssClass;
+            }
+
+            return string.Empty;
+        }
+    }
+}
--- ImproveTeam/wwwroot/js/site.js
+++ ImproveTeam/wwwroot/js/site.js
@@ -1,4 +1,26 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿$(document).ready(function () {
+    showNavbar('header-toggle', 'nav-bar', 'body-pd', 'header');
+})
 
-// Write your JavaScript code.
+function showNavbar(toggleId, navId, bodyId, headerId) {
+    const toggle = document.getElementById(toggleId);
+    const nav = document.getElementById(navId);
+    const bodypd = document.getElementById(bodyId);
+    const headerpd = document.getElementById(headerId);
+
+    if (toggle && nav && bodypd && headerpd) {
+        $(toggle).click(function () {
+            $(nav).toggleClass('show');
+            $(toggle).toggleClass('bx-x');
+            $(bodypd).toggleClass('body-pd');
+            $(headerpd).toggleClass('body-pd');
+
+            //$($.fn.dataTable.tables(true)).css('width', '100%');
+            //$($.fn.dataTable.tables(true)).DataTable().columns.adjust().draw();
+        })
+    }
+}
+
+function onLogoutBtnClick(button) {
+    $(button).closest('form').submit();
+}
