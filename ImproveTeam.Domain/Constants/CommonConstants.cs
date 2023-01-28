389f4effea163628b3b1141ff45c4e812b138d19
Oleksandr Prokopets <prokopets.alexandr@gmail.com>
2023-01-28 19:39:07 +0100

    Add empty Countries page

---
+++ ImproveTeam.Domain/Constants/CommonConstants.cs
@@ -0,0 +1,11 @@
+﻿namespace ImproveTeam.Domain.Constants
+{
+    public static class CommonConstants
+    {
+        public static class UserRole
+        {
+            public const string TeamLead = "TeamLead";
+            public const string Buyer = "Buyer";
+        }
+    }
+}
---
+++ ImproveTeam/Controllers/CountriesController.cs
@@ -0,0 +1,16 @@
+﻿using ImproveTeam.Domain.Constants;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ImproveTeam.Controllers
+{
+    [Authorize(Roles = CommonConstants.UserRole.TeamLead)]
+    public class CountriesController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
