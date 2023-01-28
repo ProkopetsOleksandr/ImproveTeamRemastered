051d5733ed26dcbbde25737bf38e209339315e0c
Oleksandr Prokopets <prokopets.alexandr@gmail.com>
2023-01-28 17:21:40 +0100

    Add AuthController and empty view for Login page

---
+++ ImproveTeam/Controllers/AuthController.cs
@@ -0,0 +1,19 @@
+﻿using ImproveTeam.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ImproveTeam.Controllers
+{
+    public class AuthController : Controller
+    {
+        [HttpGet]
+        public IActionResult Login(string returnUrl)
+        {
+            var model = new LoginViewModel
+            {
+                ReturnUrl = returnUrl
+            };
+
+            return View(model);
+        }
+    }
+}
---
+++ ImproveTeam/Models/LoginViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace ImproveTeam.Models
+{
+    public class LoginViewModel
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string ReturnUrl { get; set; }
+    }
+}
