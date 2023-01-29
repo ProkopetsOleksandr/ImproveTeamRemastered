cfc532a900b24f241419180c25443b778aad64af
Oleksandr Prokopets <prokopets.alexandr@gmail.com>
2023-01-29 17:39:23 +0100

    Add empty advertisers page

---
+++ ImproveTeam/Controllers/AdvertisersController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ImproveTeam.Controllers
+{
+    public class AdvertisersController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
